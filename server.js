baeeffce7c8877052ece8811b04bd892b5c798c5
Dileepkomatineni <dkomatin@uncc.edu>
2022-05-01 19:10:09 -0400

    commit changes

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const axios = require("axios").default;
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-const aws_fucntion_api = "https://c862zuo6ak.execute-api.us-east-1.amazonaws.com/prod/hello-resource";
+const aws_fucntion_api =
+  "https://c862zuo6ak.execute-api.us-east-1.amazonaws.com/prod/hello-resource";
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  return res.send("Server homepage");
+app.get("/name", (req, res) => {
+  return res.send("");
 });
 
 app.get("/say", (req, res) => {
